2711bc27c1e579901a538ef184308517e92e59f6
Aveheuzed <a.masson555@ntymail.com>
2021-11-14 19:17:39 +0100

    Got rid of the multiplication in `asm.s`.
    Now the two cores spend the same time computing their pointers.

--- src/asm.s
+++ src/asm.s
@@ -7,44 +7,23 @@ good:
     # %rsi -> number of lines
     # %rdx -> buffer
 
-    pushq %rbp
     pushq %rbx
-    pushq %r12
-    pushq %r13
-    pushq %r14
-    pushq %r15
+    movq $1, %r8
 
-    movq %rdx, %rbp # %rdx is going to be overwritten by MUL
-
-    xorq %rbx, %rbx # %rbx -> line number (from 0 to %rsi-1)
 .outer_loop_good:
-    xorq %rcx, %rcx # %rcx -> column number (from 0 to %rdi-1)
+    movq %rdx, %rbx
+    movq %rdi, %rcx
 .inner_loop_good:
-    # compute memory access
-    # (pointer computation could be optimized)
-    # base + line number*stride + column number
-    movq %rbx, %rax
-    mulq %rdi
-    addq %rbp, %rax
-    addq %rcx, %rax
-    movb (%rax), %al
-    # %al -> target for the MOVs
-
-    incq %rcx
-    cmpq %rcx, %rdi
-    ja .inner_loop_good
-
-    incq %rbx
-    cmpq %rbx, %rsi
-    ja .outer_loop_good
+    movb (%rbx), %al
+    addq %r8, %rbx
+    decq %rcx
+    jnz .inner_loop_good
+    addq %rdi, %rdx
+    decq %rsi
+    jnz .outer_loop_good
 
 .end_good:
-    popq %r15
-    popq %r14
-    popq %r13
-    popq %r12
     popq %rbx
-    popq %rbp
     ret
 .size good, .-good
 
@@ -56,44 +35,23 @@ bad:
     # %rsi -> number of lines
     # %rdx -> buffer
 
-    pushq %rbp
     pushq %rbx
-    pushq %r12
-    pushq %r13
-    pushq %r14
-    pushq %r15
+    movq $1, %r8
 
-    movq %rdx, %rbp # %rdx is going to be overwritten by MUL
-
-    xorq %rcx, %rcx # %rcx -> column number (from 0 to %rdi-1)
 .outer_loop_bad:
-    xorq %rbx, %rbx # %rbx -> line number (from 0 to %rsi-1)
+    movq %rdx, %rbx
+    movq %rsi, %rcx
 .inner_loop_bad:
-    # compute memory access
-    # (pointer computation could be optimized)
-    # base + line number*stride + column number
-    movq %rbx, %rax
-    mulq %rdi
-    addq %rbp, %rax
-    addq %rcx, %rax
-    movb (%rax), %al
-    # %al -> target for the MOVs
-
-    incq %rbx
-    cmpq %rbx, %rsi
-    ja .inner_loop_bad
-
-    incq %rcx
-    cmpq %rcx, %rdi
-    ja .outer_loop_bad
+    movb (%rbx), %al
+    addq %rdi, %rbx
+    decq %rcx
+    jnz .inner_loop_bad
+    addq %r8, %rdx
+    decq %rdi
+    jnz .outer_loop_bad
 
 .end_bad:
-    popq %r15
-    popq %r14
-    popq %r13
-    popq %r12
     popq %rbx
-    popq %rbp
     ret
 .size bad, .-bad
 
