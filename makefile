54bd918540a53acf1a5e7e80bdeb6b1f4ffded49
Aveheuzed <a.masson555@ntymail.com>
2021-11-17 19:22:10 +0100

    Changed algorithm for a "simpler" swipe + stats.

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 bin/benchmark: src/asm.s src/ref.s src/asmdriver.c
 	mkdir -p bin
-	clang -O2 -o $@ $^
+	clang -O2 -o $@ $^ -lm
 
 clean:
 	rm -f bin/benchmark
--- src/asmdriver.c
+++ src/asmdriver.c
@@ -1,99 +1,127 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <math.h>
 
-#define SIGNIFICANT_FACTOR 1.5
-#define NBRUNS 10
-#define MAX_SAME_CHANGES 3
-
-#define FLOOR 512L
-#define CEIL 4096L
+#define NNOTICES 3
+#define NSIGMA 5
+#define PRESAMPLES 8
+#define NBRUNS 8
 
 void ref_good(size_t stride, size_t len, char const* buffer);
 void ref_bad(size_t stride, size_t len, char const* buffer);
 void good(size_t stride, size_t len, char const* buffer);
 void bad(size_t stride, size_t len, char const* buffer);
 
-static inline int significative_at(size_t param) {
-  char* buffer = malloc(param*param);
-
+static clock_t timing_good(size_t stride, size_t len) {
+  char* buffer = malloc(stride*len);
   clock_t goodtime = 0;
+
+  ref_good(stride, len, buffer);
   goodtime += clock();
-  for (size_t i=0; i<NBRUNS; i++) ref_good(param, param, buffer);
+  for (size_t i=0; i<NBRUNS; i++) ref_good(stride, len, buffer);
   goodtime -= clock();
 
+  good(stride, len, buffer);
   goodtime -= clock();
-  for (size_t i=0; i<NBRUNS; i++) good(param, param, buffer);
+  for (size_t i=0; i<NBRUNS; i++) good(stride, len, buffer);
   goodtime += clock();
-
-
-  clock_t badtime = 0;
-  badtime += clock();
-  for (size_t i=0; i<NBRUNS; i++) ref_bad(param, param, buffer);
-  badtime -= clock();
-
-  badtime -= clock();
-  for (size_t i=0; i<NBRUNS; i++) bad(param, param, buffer);
-  badtime += clock();
-
 
   free(buffer);
 
-  printf("for param %lu\n\tgood:\t%ld\n\tbad:\t%ld\n", param, goodtime, badtime);
-
-  return badtime > SIGNIFICANT_FACTOR * goodtime;
+  return goodtime;
 }
 
-static inline size_t dichotomy(size_t low, size_t high) {
-  // almost a plain dichotomy; we monitor the change tendency in addition.
-  // when the tendency stays, we accelerate things up by pushing the other bound
+static clock_t timing_bad(size_t stride, size_t len) {
+  char* buffer = malloc(stride*len);
+  clock_t badtime = 0;
 
-  // this is implemented in order to become noise resistant!
+  ref_bad(stride, len, buffer);
+  badtime += clock();
+  for (size_t i=0; i<NBRUNS; i++) ref_bad(stride, len, buffer);
+  badtime -= clock();
 
-  int changes_tendency = 0; // ++ when updating low, -- when updating high
-  while (high-low > 2) {
-    size_t mid = (high+low)/2;
-    if (significative_at(mid)) {
-      puts("Value above cache size");
-      high = mid;
-      if (changes_tendency<=0) changes_tendency--;
-      else changes_tendency = -1;
-    }
-    else {
-      puts("Value below cache size");
-      low = mid;
-      if (changes_tendency>=0) changes_tendency++;
-      else changes_tendency = +1;
-    }
+  bad(stride, len, buffer);
+  badtime -= clock();
+  for (size_t i=0; i<NBRUNS; i++) bad(stride, len, buffer);
+  badtime += clock();
 
-    if (abs(changes_tendency) > MAX_SAME_CHANGES) {
-      puts("Adjustment triggers!");
-      if (changes_tendency < 0) {
-        // push `low` away
-        low -= (high-low);
-      }
-      else {
-        // push `high` away
-        high += (high-low);
-      }
-      changes_tendency = 0;
-    }
-  }
-  return (low+high)/2;
+  free(buffer);
+
+  return badtime;
 }
 
-int main(int argc, char* argv[]) {
+static float indicator(float goodtime, float badtime) {
+  return expf((badtime - goodtime) / (badtime + goodtime));
+}
+
+static void print_notice(float bufsize) {
   static const char* prefixes[] = {"B", "KiB", "MiB", "GiB", "ZiB", "YiB"};
 
-
-  float approx = dichotomy(FLOOR, CEIL);
-  approx *= approx;
-
   int i = 0;
-  while (approx > 1024) {
-    approx /= 1024;
+  while (bufsize > 1024) {
+    bufsize /= 1024;
     i++;
   }
 
-  printf("Estimated L3 buffer size: %.1f %s\n", approx, prefixes[i]);
+  printf("Mark at %.1f %s\n", bufsize, prefixes[i]);
+}
+
+static void preheat(size_t inner, size_t outer, float *const avg, float *const stddev) {
+  float sum = 0;
+  float sumsq = 0;
+
+  for (int n=0; n<PRESAMPLES; n++) {
+    clock_t goodtime = timing_good(inner, outer);
+    clock_t badtime = timing_bad(inner, outer);
+    float new = indicator(goodtime, badtime);
+    sum += new;
+    sumsq += new*new;
+  }
+
+  sum /= PRESAMPLES; // average
+  sumsq /= PRESAMPLES; // average of the squares
+  *avg = sum;
+  *stddev = 10*sqrtf((sumsq - sum*sum)/(PRESAMPLES-1));
+}
+
+static int swipe(size_t innerdim, size_t inner_inc, size_t outerdim) {
+  fprintf(stderr, "Swipe with outer dim %lu\n", outerdim);
+
+  float avg;
+  float stddev;
+
+  preheat(innerdim, outerdim, &avg, &stddev);
+
+  fprintf(stderr, "Avg: %.3f // stddev: %.3f\n", avg, stddev);
+
+  unsigned nnotices = 0;
+  clock_t goodtime, badtime;
+  do {
+    innerdim += inner_inc;
+    goodtime = timing_good(innerdim, outerdim);
+    badtime = timing_bad(innerdim, outerdim);
+    fprintf(stderr, "indicator for (%lu, %lu): %.3f ", innerdim, outerdim, indicator(goodtime, badtime));
+    if (/*fabs*/(indicator(goodtime, badtime) - avg) > NSIGMA*stddev) {
+      fprintf(stderr, "*\n");
+      nnotices++;
+    }
+    else {
+      fprintf(stderr, "\n");
+      nnotices = 0;
+    }
+  } while (innerdim < outerdim*(2*2+1) && nnotices < NNOTICES);
+
+  if (nnotices >= NNOTICES) {
+    print_notice(innerdim*outerdim);
+    return 0;
+  }
+  else {
+    return 1;
+  }
+  // return nnotices < NNOTICES;
+}
+
+int main(int argc, char* argv[]) {
+  for (size_t dim=512; swipe(dim, dim/4, dim); dim*=2);
 }
